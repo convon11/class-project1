906bba1bb52c2d5b7b87ee6efb28369bd5b27d13
convon <coreynordloh@nordloh.com>
2016-02-05 12:26:57 -0700

    Update scedual.py

--- md/scedual.py
+++ md/scedual.py
@@ -1,6 +1,8 @@
 mstimesone=print(import"the time it will happen is:")
 mstimestwo=print(import"the time it will happen is:")
 mstimesthree=print(import"the time it will happen is:")
+mstimesfour=print(import"the time it will happen is:")
+mstimesfive=print(import"the time it will happen is:")
 
 import time()
 t=time()
@@ -12,6 +14,11 @@ def scedual():
   print(mstimestwo)
   print(import"the third item on the scedual is:")
   print(mstimesthree)
+  print(import"the fourth item on the scedual is:")
+  print(mstimesfour)
+  print(import"the fifth item on the scedual is:")
+  print(mstimesfive)
+
 
 tk=Tkinter()
 create_scedual=Tkinter.Button(tk,text="create scedual",command=scedual)
