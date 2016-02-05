ee32bf76b48a6955ef3ad42fafe86ecfa16ba5f1
convon <coreynordloh@nordloh.com>
2016-02-05 12:10:00 -0700

    create scedual update

--- md/scedual.py
+++ md/scedual.py
@@ -1,3 +1,11 @@
 import time()
 t=time()
-from tkinter import*
+from Tkinter import*
+def scedual():
+  print(import"the first item on the scedual is:")
+  print(import"the time it will happen is:")
+
+tk=Tkinter()
+create_scedual=Tkinter.Button(tk,text="create scedual",command=scedual)
+create_scedual.pack
+tk.mainloop()
